c7c8eb2bc92a4e69a65664440deb16b192f09dc2
hanteo <hanwei.teo@gmail.com>
2017-03-01 15:16:02 +0000

    Green

---
+++ CashCard.Tests/Tests.cs
@@ -0,0 +1,115 @@
+﻿using NUnit.Framework;
+
+namespace CashCard.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void DefaultUnauthenticated()
+        {
+            var pin = new[] {1, 2, 3, 4};
+            var card = new PrepaidCard(pin);
+
+            Assert.False(card.Authenticated);
+        }
+
+        [Test]
+        public void DefaultZeroBalance()
+        {
+            var pin = new[] {1, 2, 3, 4};
+            var card = new PrepaidCard(pin);
+
+            Assert.AreEqual(0, card.Balance);
+        }
+
+        [Test]
+        public void AuthenticatedWithWrongPinRemainsUnauthenticated()
+        {
+            var pin = new[] {1, 2, 3, 4};
+            var card = new PrepaidCard(pin);
+
+            card.Authenticate(new[] {0, 0, 0, 0});
+
+            Assert.False(card.Authenticated);
+        }
+
+        [Test]
+        public void AuthenticatedWithCorrectPinAuthenticates()
+        {
+            var pin = new[] {1, 2, 3, 4};
+            var card = new PrepaidCard(pin);
+
+            card.Authenticate(pin);
+
+            Assert.True(card.Authenticated);
+        }
+
+        [Test]
+        public void CannotAddIfUnauthenticated()
+        {
+            var pin = new[] {1, 2, 3, 4};
+            var card = new PrepaidCard(pin);
+            var isAdded = card.Add(10);
+
+            Assert.False(isAdded);
+            Assert.AreEqual(0, card.Balance);
+        }
+
+        [Test]
+        public void CanAddIfAuthenticated()
+        {
+            var pin = new[] {1, 2, 3, 4};
+            var card = new PrepaidCard(pin);
+
+            var isAuthenticated = card.Authenticate(pin);
+            var isAdded = card.Add(10);
+
+            Assert.True(isAuthenticated);
+            Assert.True(isAdded);
+            Assert.AreEqual(10, card.Balance);
+        }
+
+        [Test]
+        public void CannotWithdrawIfUnauthenticated()
+        {
+            var pin = new[] {1, 2, 3, 4};
+            var card = new PrepaidCard(pin);
+
+            var canWithdraw = card.Withdraw(10);
+
+            Assert.False(canWithdraw);
+            Assert.AreEqual(0, card.Balance);
+        }
+
+        [Test]
+        public void CanWithdrawIfAuthenticatedAndSufficientAmount()
+        {
+            var pin = new[] {1, 2, 3, 4};
+            var card = new PrepaidCard(pin);
+
+            card.Authenticate(pin);
+            var isAdded = card.Add(20);
+            var canWithdraw = card.Withdraw(10);
+
+            Assert.True(isAdded);
+            Assert.True(canWithdraw);
+            Assert.AreEqual(10, card.Balance);
+        }
+
+        [Test]
+        public void CannotWithdrawIfAuthenticatedButInsufficientAmount()
+        {
+            var pin = new[] {1, 2, 3, 4};
+            var card = new PrepaidCard(pin);
+
+            card.Authenticate(pin);
+            var isAdded = card.Add(10);
+            var canWithdraw = card.Withdraw(20);
+
+            Assert.True(isAdded);
+            Assert.False(canWithdraw);
+            Assert.AreEqual(10, card.Balance);
+        }
+    }
+}
---
+++ PrepaidCard.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+
+namespace CashCard
+{
+    public class PrepaidCard
+    {
+        private readonly int[] _pin;
+        private volatile int _balance;
+        private volatile bool _authenticated;
+        private readonly object _locker;
+
+        public bool Authenticated => _authenticated;
+        public int Balance => _balance;
+
+        public PrepaidCard(int[] pin)
+        {
+            _pin = pin;
+            _locker = new object();
+        }
+
+        public bool Authenticate(int[] pin)
+        {
+            lock (_locker)
+            {
+                if (!pin.SequenceEqual(_pin))
+                {
+                    return false;
+                }
+
+                _authenticated = true;
+            }
+
+            return true;
+        }
+
+        public bool Add(int amount)
+        {
+            lock(_locker)
+            {
+                if (!Authenticated)
+                {
+                    return false;
+                }
+
+                if (amount <= 0)
+                {
+                    return false;
+                }
+
+                _balance += amount;
+            }
+
+            return true;
+        }
+
+        public bool Withdraw(int amount)
+        {
+            lock (_locker)
+            {
+                if (!Authenticated)
+                {
+                    return false;
+                }
+
+                if (Balance < amount)
+                {
+                    return false;
+                }
+
+                _balance -= amount;
+            }
+
+            return true;
+        }
+    }
+}
